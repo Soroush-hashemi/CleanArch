47d0c098df38961495de28f7e0e9222a30bf2c39
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2023-07-26 13:20:39 +0430

    create ProductImage make Product class for Root Aggregate

--- CleanArch/Domain/Entities/ProductAgg/Product.cs
+++ CleanArch/Domain/Entities/ProductAgg/Product.cs
@@ -7,6 +7,7 @@ namespace Domain.Entities
         public Guid Id { get; private set; } // نباید از بیرون این متغییر ها قابل تغییر باشن پس از پرایوت استفاده میکنم 
         public Money Price { get; private set; }
         public string Title { get; private set; }
+        public ICollection<ProductImages> Images { get; private set; }
 
         // پراپرتی های بالا به دلیل پرایوت بودن ست نمیشن و نال میمونن پی باید از این کانستراکتور استفاده بکنیم
         public Product(string title, Money price) 
@@ -22,6 +23,20 @@ namespace Domain.Entities
             Guard(title , price);
             Title = title;
             Price = price;
+        }
+
+        public void RemoveImage(long Id)
+        {
+            var image = Images.FirstOrDefault(i => i.Id == Id);
+            if (image != null)
+                throw new InvalidOperationException("Image Is null");
+                
+            Images.Remove(image);
+        }
+
+        public void AddImage(string ImageName)
+        {
+            Images.Add(new ProductImages(Id,ImageName));
         }
 
         private void Guard(string title, Money price)
---
+++ CleanArch/Domain/Entities/ProductAgg/ProductImages.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain;
+
+public class ProductImages
+{
+    public string ImageName { get; private set; }
+    public long Id { get; private set; }
+    public Guid ProductId { get; private set; }
+
+    public ProductImages(Guid productId, string imageName)
+    {
+        if (productId == null)
+            throw new ArgumentNullException(nameof(productId));
+
+        if (imageName == null)
+            throw new ArgumentNullException(nameof(imageName));
+        
+        productId = ProductId;
+        imageName = ImageName;
+    }
+}
