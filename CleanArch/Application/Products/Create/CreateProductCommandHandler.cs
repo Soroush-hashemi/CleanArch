a11433b48c5086f99c5b6075e1cf100d85b45a5b
soroush Hashemi <84335590+Soroush-hashemi@users.noreply.github.com>
2023-08-16 21:31:29 +0430

    CreateProductCommandValidator

--- CleanArch/Application/Products/Create/CreateProductCommandHandler.cs
+++ CleanArch/Application/Products/Create/CreateProductCommandHandler.cs
@@ -8,13 +8,20 @@ namespace Application.Command.Products.Create
     public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, long>
     {
         private readonly IProductRepository _repository;
-        public CreateProductCommandHandler(IProductRepository repository)
+        private readonly CreateProductCommandValidator _validations;
+        public CreateProductCommandHandler(IProductRepository repository, CreateProductCommandValidator validations)
         {
             _repository = repository;
+            _validations = validations;
+
         }
 
         public async Task<long> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
+            var ValidatorChecker = _validations.Validate(request);
+            if (!ValidatorChecker.IsValid)
+                throw new InvalidDataException(ValidatorChecker.Errors.ToString());
+
             var product = new Product(request.Title, Money.FromRial(request.Price));
             _repository.Add(product);
             await _repository.SaveChanges();
---
+++ CleanArch/Application/Products/Create/CreateProductCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿
+using FluentValidation;
+
+namespace Application.Command.Products.Create
+{
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(t => t.Title)
+                .MinimumLength(3).WithMessage("عنوان باید حداقل 3 کاراکتر باشد")
+                .MaximumLength(50).WithMessage("عنوان باید حداثل 50 کاراکتر باشد");
+
+            RuleFor(p => p.Price)
+                .GreaterThan(0).WithMessage("قیمت باید بیشتر از 0 باشد");
+                
+        }
+    }
+}
